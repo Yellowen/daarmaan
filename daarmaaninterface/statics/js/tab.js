1ca6ac76f24bf4c71403cda495a09a63a62c9f7c
Sameer Rahmani <lxsameer@gnu.org>
2012-11-28 20:02:20 +0330

    JS bug fixed

--- daarmaaninterface/statics/js/tab.js
+++ daarmaaninterface/statics/js/tab.js
@@ -1,24 +1,40 @@
+function show_login () {
+    $("#loginform").hide();
+    $("#registerform").show();
+    $("#logintab").removeClass("tab selectedtab").addClass("tab unselected");
+    $("#registertab").removeClass("tab unselected").addClass("tab selectedtab");
+
+};
+
+function show_register () {
+    $("#registerform").hide();
+    $("#loginform").show();
+    $("#registertab").removeClass("tab selectedtab").addClass("tab unselected");
+    $("#logintab").removeClass("tab unselected").addClass("tab selectedtab");
+};
+
 function hashchange (){
     hash = window.location.hash;
-    console.log(hash);
+
     if (hash == "#register") {
-	$("#loginform").hide();
-	$("#registerform").show();
-	$("#logintab").removeClass("tab selectedtab").addClass("tab unselected");
-	$("#registertab").removeClass("tab unselected").addClass("tab selectedtab");
+	show_login();
     }
     else {
-	$("#registerform").hide();
-	$("#loginform").show();
-	$("#registertab").removeClass("tab selectedtab").addClass("tab unselected");
-	$("#logintab").removeClass("tab unselected").addClass("tab selectedtab");
+	show_register();
     }
 
 };
+
 $(function(){
     window.onhashchange = hashchange;
-
-
+    
+    hash = window.location.hash;
+    if (hash == "#register") {
+	show_login();
+    }
+    else {
+	show_register();
+    }
     $("#logintab").click(function(){
 	if ($(this).hasClass("selectedtab") == false) {
 	    $(this).removeClass("tab unselected").addClass("tab selectedtab");
