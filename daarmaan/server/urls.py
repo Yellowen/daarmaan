4772481b13a2e8a06ab18f7d4776f721979a06a5
Sameer Rahmani <lxsameer@gnu.org>
2012-11-30 12:32:41 +0330

    reset password templates added

--- daarmaan/server/urls.py
+++ daarmaan/server/urls.py
@@ -33,6 +33,23 @@ urlpatterns = patterns('',
         "daarmaan.server.views.statics.ajax_widget_jsonp",
         name="ajax-widget-jsonp"),
 
+    # Password reset -------------------------
+    url(r'^password_reset/$',
+        'django.contrib.auth.views.password_reset',
+        name='password_reset'),
+
+    url(r'^password_reset/done/$',
+        'django.contrib.auth.views.password_reset_done',
+        name='password_reset_done'),
+
+    url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        name='password_reset_confirm'),
+    url(r'^reset/done/$',
+        'django.contrib.auth.views.password_reset_complete',
+        name='password_reset_complete'),
+    # ----------------------------------------
+
     url(r"^me/", include(profile.urls)),
     url(r"^\~([A-Za-z][^/]+)/$", profile.view_profile,
         name="view_user_profile"),
