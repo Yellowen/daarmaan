6b12d36c8e2e54ed58180da214f95c3754fa14cd
Sameer Rahmani <lxsameer@gnu.org>
2012-12-20 09:53:53 +0330

    Last changes for widget in widget area

--- daarmaan/server/widgetset/buttons.py
+++ daarmaan/server/widgetset/buttons.py
@@ -16,10 +16,16 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # -----------------------------------------------------------------------------
+import json
 
 from django.utils.translation import ugettext as _
+from django.shortcuts import render_to_response as rr
+from django.template import RequestContext
+from django.http import HttpResponse
 from vanda.apps.dashboard.widgets.button import Button
 from vanda.apps.dashboard.base import dashboard
+
+from forms.config import ConfigForm
 
 
 class UserButton (Button):
@@ -30,20 +36,19 @@ class UserButton (Button):
     weight = 1000
 
     css = "/statics/css/bar_button.css"
-    js = ["/statics/js/jquery.min.js",
-          "/statics/js/bar_button.js"]
+    #js = "/statics/js/bar_button.js"
 
     css_class = "bar_button user_button"
     template = "dashboard/widgets/user_button.html"
 
     def text(self):
-        return self.dashboard.request.user.username
+        return self.request.user.username
 
     def image_src(self):
-        return self.dashboard.request.user.email
+        return self.request.user.email
 
     def host(self):
-        return self.dashboard.request.META["HTTP_HOST"]
+        return self.request.META["HTTP_HOST"]
 
 
 class SettingButton (UserButton):
@@ -53,15 +58,68 @@ class SettingButton (UserButton):
     gravatar = False
     weight = 900
     template = "dashboard/widgets/settings_button.html"
+    form = ConfigForm
+
+    @property
+    def url_patterns(self):
+        from django.conf.urls import url
+        return [
+            url('^config/form/$', self.config_form),
+            ]
+
     def text(self):
         return False
 
     def image_src(self):
         return "/statics/image/settings.png"
 
+    # Views -------------------------------
+    def config_form(self, request):
+        if request.method == "POST":
+            pass
+        else:
+            form = self.form()
+            return rr(form.template,
+                    {"form": form,
+                     "self": self},
+                    context_instance=RequestContext(request))
+
+
+class AddWidgetButton (UserButton):
+    title = _("Widget Menu")
+    name = "add_widget"
+    image = True
+    gravatar = False
+    weight = 800
+    template = "dashboard/widgets/settings_button.html"
+
+    @property
+    def url_patterns(self):
+        from django.conf.urls import url
+        return [
+            url('^list/$', self.widgets_list),
+            ]
+
+    def text(self):
+        return False
+
+    def image_src(self):
+        return "/statics/image/add.png"
+
+    # Views -------------------------------
+    def widgets_list(self, request):
+        widgets = list(self.dashboard.widgets)
+        #widgets = [i.name for i in widgets]
+        return rr("dashboard/widgets/add_widgets.html",
+                  {"widgets": widgets,
+                   "self": self},
+                  context_instance=RequestContext(request))
+
 
 dashboard.register(UserButton())
 dashboard.add_widget_to('header', UserButton())
+dashboard.register(AddWidgetButton())
+dashboard.add_widget_to('header', AddWidgetButton())
 dashboard.register(SettingButton())
 dashboard.add_widget_to('header', SettingButton())
 
--- daarmaaninterface/statics/js/bar_button.js
+++ daarmaaninterface/statics/js/bar_button.js
@@ -5,10 +5,5 @@ $(function(){
     }, function(){
 	$("#id_setting_button img").attr("src", "/statics/image/settings.png");
     });
-    $(".bar_button").click(function(){
-	var menu = $(".submenu[belongs_to='" + $(this).attr("id") + "']");
-	$(".submenu").not(menu).hide();
-	menu.fadeToggle("slow");
-    });
 
 });
