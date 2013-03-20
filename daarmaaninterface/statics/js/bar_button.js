5bd4b03fb57faa70ca1daaf0657465c6b22c74e7
Sameer Rahmani <lxsameer@gnu.org>
2013-03-20 11:11:07 +0330

    Source tree cleaned

--- daarmaaninterface/statics/js/bar_button.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("#id_setting_button").hover(function(){
-	$("#id_setting_button img").attr("src", "/statics/image/settings-hover.png");
-	
-    }, function(){
-	$("#id_setting_button img").attr("src", "/statics/image/settings.png");
-    });
-
-});
